9a0d4418ee4f40570360ba8b41410462f9d70a94
Jeff Lu <ljeff@us.ibm.com>
2021-07-19 14:34:14 -0400

    pipe child process to parent console

--- saved-model-service/find-node.js
+++ saved-model-service/find-node.js
@@ -6,10 +6,14 @@ const find = (name) => {
   findProcess('name', name, true)
   .then(function (list) {
     if(!list.find(exist)) {
-      exec('node index.js', {maxBuffer: 1024 * 2000}, (err, stdout, stderr) => {
+      let child = exec('node index.js', {maxBuffer: 1024 * 2000}, (err, stdout, stderr) => {
         console.log('restarting node index.js')
         console.log('there are %s node process(es)', list.length);
       });
+      child.stdout.pipe(process.stdout);
+      child.on('data', (data) => {
+        console.log(data)
+      })
     } 
   });
 }
