28c7415d7975ed86f7c217d7b2b6e9414ac527f8
Jeff Lu <ljeff@us.ibm.com>
2021-07-27 10:17:14 -0400

    check if dirs exists

--- saved-model-service/find-node.js
+++ saved-model-service/find-node.js
@@ -12,7 +12,7 @@ const find = (name) => {
       let child = exec('node index.js', {maxBuffer: 1024 * 2000}, (err, stdout, stderr) => {
         console.log('restarting node index.js')
         console.log('there are %s node process(es)', list.length);
-        sleep(10000).then(() => {
+        sleep(5000).then(() => {
           setCheckInterval(10000);
         })
       });
